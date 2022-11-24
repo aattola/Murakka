5a734d790e985872473490f9f30d361558e8ec40
JEFFe <leevi@i.jeffe.co>
2022-11-24 16:53:46 +0200

    chore: parempi tyylitys

--- src/commands/gamba/tiili.ts
+++ src/commands/gamba/tiili.ts
@@ -35,9 +35,8 @@ export class TiliCommand extends Command {
 
     const tiliFormatted = tili
       .map((row) => {
-        return `${formatDistanceToNow(new Date(row.timestamp), { locale: fi })} sitten: ${
-          row.rahat
-        }€`
+        const date = formatDistanceToNow(new Date(row.timestamp), { locale: fi })
+        return `${date} sitten: ${row.rahat}€`
       })
       .join(`\n`)
 
