429bea3cf61e8d7887fddc871cb17eaab0c89009
JEFFe <leevi@i.jeffe.co>
2022-11-24 15:50:03 +0200

    fix: sentry tracing

--- src/init/sentry.ts
+++ src/init/sentry.ts
@@ -13,12 +13,11 @@ declare module '@sapphire/pieces' {
 function initSentry() {
   Sentry.init({
     dsn: 'https://ed8634031d35418f9c86536413fa8986@o124657.ingest.sentry.io/6710413',
-    tracesSampleRate: 1.0,
     integrations: [
-      new Sentry.Integrations.Http({ tracing: true }),
       new Tracing.Integrations.Prisma({ client: container.prisma }),
       new ProfilingIntegration()
     ],
+    tracesSampleRate: 1.0,
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     profilesSampleRate: 1.0
