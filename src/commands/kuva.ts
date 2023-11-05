9a6149975ec3cfca5aa12765c0925f2f2ef01f45
JEFFe <leevi@i.jeffe.co>
2023-11-05 05:40:42 +0200

    feat: oma ai generointi

--- src/commands/kuva.ts
+++ src/commands/kuva.ts
@@ -1,7 +1,15 @@
 import { ApplyOptions } from '@sapphire/decorators'
 import { BucketScope, Command } from '@sapphire/framework'
-import { createImage, getPrompt } from '../lib/chatgpt'
+import { createImage } from '../lib/chatgpt'
 import { AttachmentBuilder } from 'discord.js'
+import { fetch } from 'undici'
+import { RecordType } from 'zod'
+
+interface txt2imgRes {
+  images: string[]
+  parameters: RecordType<any, any>
+  info: string
+}
 
 @ApplyOptions<Command.Options>({
   name: 'kuva',
@@ -28,24 +36,62 @@ export class UserCommand extends Command {
     )
   }
 
+  private async fetchImage(prompt: string) {
+    const res = await fetch('https://ai.jeffe.co/sdapi/v1/txt2img', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt
+      })
+    })
+
+    if (res.status === 422) throw new Error('Hei joku meni mönkään (validation error)')
+
+    const data = (await res.json()) as txt2imgRes
+
+    return data.images[0]
+  }
+
   public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
     await interaction.deferReply({ ephemeral: false })
     if (!interaction.inCachedGuild()) throw new Error('Rare en tunne guildia error')
     const teksti = interaction.options.getString('teksti')!
 
-    const vastaus = await createImage(teksti)
+    // const vastaus = await createImage(teksti)
+    const vastaus = await this.fetchImage(teksti).catch(async () => {
+      return {
+        fallback: true,
+        image: await createImage(teksti)
+      }
+    })
 
     if (vastaus === undefined) {
       return interaction.editReply({
         content: 'Virhe tapatui kuvaa generoidessa :('
       })
     }
-    const kuva = new AttachmentBuilder(vastaus, {
-      name: 'AI Generated kuva.png'
-    })
+
+    let kuva
+    let isFallback = false
+
+    if (typeof vastaus === 'object') {
+      isFallback = true
+      kuva = new AttachmentBuilder(vastaus.image!, {
+        name: 'AI Generated kuva.png'
+      })
+    } else {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const buf = new Buffer.from(vastaus, 'base64')
+      kuva = new AttachmentBuilder(buf, {
+        name: 'AI Generated kuva.png'
+      })
+    }
 
     await interaction.editReply({
-      content: `> ${teksti}`,
+      content: `> ${teksti} ${isFallback && '(OpenAI Generoima)'}`,
       files: [kuva]
     })
   }
