fa22f3786dfe15be11ea65e738db5f6038d63dec
JEFFe <jeffe@jeffe.co>
2026-03-09 19:14:04 +0200

    fix: tankille vol 3

--- src/init/tankille.ts
+++ src/init/tankille.ts
@@ -1,9 +1,12 @@
 import { Client } from '@jeffe/tankille'
 
-const tankille = new Client({
-  device: 'Pixel 7 Pro (f9a9b2d3d4251234)',
-  userAgent: 'FuelFellow/3.9.19 (null)'
-})
+const tankille = new Client(
+  {
+    device: 'Pixel 7 Pro (f9a9b2d3d4251234)',
+    userAgent: 'FuelFellow/3.9.19 (null)'
+  },
+  { 'User-Agent': 'FuelFellow/3.9.19 (null)' }
+)
 
 if (!process.env.TANKILLEPASS) throw new Error('Ei tankille pass env olemassa')
 const [email, password] = process.env.TANKILLEPASS.split(':')
