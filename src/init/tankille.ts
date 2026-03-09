3e49f12a7d1b18838458334ed0c4f577d6bf4c25
JEFFe <jeffe@jeffe.co>
2026-03-09 18:54:42 +0200

    fix: tankille api :)

--- src/init/tankille.ts
+++ src/init/tankille.ts
@@ -1,6 +1,9 @@
 import { Client } from '@jeffe/tankille'
 
-const tankille = new Client()
+const tankille = new Client({
+  device: 'Pixel 7 Pro (f9a9b2d3d4251234)',
+  userAgent: 'FuelFellow/3.9.19 (null)'
+})
 
 if (!process.env.TANKILLEPASS) throw new Error('Ei tankille pass env olemassa')
 const [email, password] = process.env.TANKILLEPASS.split(':')
